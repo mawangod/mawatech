339d99d1c1aea1288ecf221c245be5f91b50a27c
mawangod <mawango777@gmail.com>
2020-09-10 11:25:28 +0200

    try to add custom design

---
+++ components/header.vue
@@ -0,0 +1,93 @@
+<template>
+	<header>
+		<!-- Header Start -->
+		<div class="header-area">
+			<div class="main-header">
+				<div class="header-top d-none d-lg-block">
+					<div class="container">
+						<div class="col-xl-12">
+							<div
+								class="row d-flex justify-content-between align-items-center"
+							>
+								<div class="header-info-left">
+									<ul>
+										<li>
+											<i class="far fa-clock"></i> Mon - SAT: 6.00 am - 10.00 pm
+										</li>
+										<li>Sun: Closed</li>
+									</ul>
+								</div>
+								<div class="header-info-right">
+									<ul class="header-social">
+										<li>
+											<a href="#"><i class="fab fa-facebook-f"></i></a>
+										</li>
+										<li>
+											<a href="#"><i class="fab fa-twitter"></i></a>
+										</li>
+										<li>
+											<a href="#"><i class="fab fa-linkedin-in"></i></a>
+										</li>
+										<li>
+											<a href="#"><i class="fab fa-google-plus-g"></i></a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header-bottom header-sticky">
+					<div class="container">
+						<div class="row align-items-center">
+							<!-- Logo -->
+							<div class="col-xl-2 col-lg-2">
+								<div class="logo">
+									<a href="index.html"
+										><img src="assets/img/logo/logo.png" alt=""
+									/></a>
+								</div>
+							</div>
+							<div class="col-xl-10 col-lg-10">
+								<div
+									class="menu-wrapper d-flex align-items-center justify-content-end"
+								>
+									<!-- Main-menu -->
+									<div class="main-menu d-none d-lg-block">
+										<nav>
+											<ul id="navigation">
+												<li><a href="index.html">Home</a></li>
+												<li><a href="about.html">About</a></li>
+												<li><a href="services.html">Services</a></li>
+												<li>
+													<a href="blog.html">Blog</a>
+													<ul class="submenu">
+														<li><a href="blog.html">Blog</a></li>
+														<li>
+															<a href="blog_details.html">Blog Details</a>
+														</li>
+														<li><a href="elements.html">Element</a></li>
+													</ul>
+												</li>
+												<li><a href="contact.html">Contact</a></li>
+											</ul>
+										</nav>
+									</div>
+								</div>
+							</div>
+							<!-- Mobile Menu -->
+							<div class="col-12">
+								<div class="mobile_menu d-block d-lg-none"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Header End -->
+	</header>
+</template>
+
+<script>
+export default {}
+</script>
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+	extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+	// add your custom config here
+	// https://stylelint.io/user-guide/configuration
+	rules: {
+		'at-rule-no-unknown': null,
+		'no-descending-specificity': null
+	},
+	ignoreFiles: ['./assets/sass/herited-style.scss']
 }
