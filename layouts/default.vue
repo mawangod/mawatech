e99eafb1180230900a29881344b7b5e97975145f
mawangod <mawango777@gmail.com>
2020-09-22 13:06:48 +0200

    go on top button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="body-bg body" style="overflow: visible;">
-		<Header />
+	<div class="body-bg body-t" style="overflow: visible;">
+		<Header id="header-t" />
 		<Nuxt />
 		<Footer />
-		<div id="back-top">
+		<div id="back-top" @click="scrollToTheTop">
 			<a title="Go to Top" href="#">
 				<FontAwesomeIcon class="fas" :icon="['fas', 'level-up-alt']">
 				</FontAwesomeIcon>
@@ -13,11 +13,20 @@
 </template>
 
 <script>
-export default {}
+export default {
+	methods: {
+		scrollToTheTop() {
+			const element = document.getElementById('header-t')
+			this.$smoothScroll({
+				scrollTo: element
+			})
+		}
+	}
+}
 </script>
 
 <style>
-.body {
+.body-t {
 	font-family: 'Poppins', sans-serif;
 	font-weight: normal;
 	font-style: normal;
--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
 	 ** Plugins to load before mounting the App
 	 ** https://nuxtjs.org/guide/plugins
 	 */
-	plugins: [],
+	plugins: ['@/plugins/vue-smooth-scroll'],
 	/*
 	 ** Auto import components
 	 ** See https://nuxtjs.org/api/configuration-components
