1cf8ee622fdbacf270e1c62b9e457f9904ec3fe3
mawangod <mawango777@gmail.com>
2020-09-28 14:11:36 +0200

    post page start i18n & refactor

---
+++ components/post-info.vue
@@ -0,0 +1,33 @@
+<template>
+	<ul class="blog-info-link">
+		<li>
+			<a href="#">
+				<FontAwesomeIcon class="fa" :icon="['fas', 'user']"> </FontAwesomeIcon>
+				<span>{{ $t(`blog.${tags[0]}`) }},</span>
+				<span>{{ $t(`blog.${tags[1]}`) }}</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<FontAwesomeIcon class="fa" :icon="['fas', 'comments']">
+				</FontAwesomeIcon>
+				{{ comments.length }} {{ $t('blog.comments') }}
+			</a>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		tags: {
+			type: Array,
+			required: true
+		},
+		comments: {
+			type: Array,
+			required: true
+		}
+	}
+}
+</script>
