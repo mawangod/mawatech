35522031ab5122b44760d4e176f96c2d1ddfbb60
mawangod <mawango777@gmail.com>
2020-11-30 12:39:36 +0100

    try again

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 const mongodbPass = process.env.MONGODB_PASSWORD
-const url = `mongodb+srv://mawatech:${mongodbPass}@mycluster.f68qp.mongodb.net/mawatech`
+const url = `mongodb+srv://mawatech:${mongodbPass}@mycluster.f68qp.mongodb.net/mawatech?retryWrites=true&w=majority`
 
 export {url}
