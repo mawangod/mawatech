34cfcfe3035fa7c4989d99816a4f971d0ce2e252
mawangod <mawango777@gmail.com>
2021-04-06 18:50:59 +0200

    upgrade-dependencies

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
 		'^~/(.*)$': '<rootDir>/$1',
 		'^vue$': 'vue/dist/vue.common.js'
 	},
+	modulePathIgnorePatterns: ['<rootDir>/cypress/'],
 	moduleFileExtensions: ['js', 'vue', 'json'],
 	transform: {
 		'^.+\\.js$': 'babel-jest',
